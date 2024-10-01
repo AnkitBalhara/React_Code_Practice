c828564388440dfde25a364ef61ddba37a0acf2f
Ankit Balhara <ankitbalhara012@gmail.com>
2024-10-01 15:46:05 +0530

    Creating Netflix Clone in React

---
+++ Netflix_Clone/src/App.jsx
@@ -0,0 +1,27 @@
+const RamRam = () => {
+  return (
+    <div id="content">
+      <img
+        src="../public/jai-shri-ram-images_9.webp"
+        width="20%"
+        alt="Jai Shree Ram"
+      />
+      <h1>Jai Shree Ram</h1>
+      <p>Ram Bhagat Hanuman</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <RamRam />
+      <RamRam />
+      <RamRam />
+      <RamRam />
+      <RamRam />
+    </>
+  );
+};
+
+export default App;
