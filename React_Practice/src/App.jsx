37b6c3c1b96da7a6fdead02b17a6826575f62de1
Ankit Balhara <ankitbalhara012@gmail.com>
2025-10-25 21:45:08 +0530

    cleared for interviews

--- React_Practice/src/App.jsx
+++ React_Practice/src/App.jsx
@@ -1,55 +1,11 @@
-// //============Passing Props from Child to the Parent Component------------------------------
-import React, { useState } from "react";
+import React from 'react'
 
 const App = () => {
-  const [name, setName] = useState();
-  const onInputChange = (value) => {
-    setName(value);
-  };
   return (
     <div>
-      <h3>This is a Parent Component...</h3>
-      <p>Calling Prop from Child components :- <strong>{name}</strong></p>
-
-      <ChildComponent username={onInputChange} />
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
-
-const ChildComponent = ({ username }) => {
-  return (
-    <div>
-        <h3>Child Component Starts....</h3>
-      <input
-        type="text"
-        onChange={(e) => {username(e.target.value)}}/>
-    </div>
-  );
-};
-
-// //------------Passing props from Parent to the child Component----------------
-
-// import React, { useState } from "react";
-
-// const App = () => {
-//     const [name, setname] = useState()
-//   return <div>This is a Normal Div in the Container
-//     <div>
-//         <input type="text" name="" value={name} onChange={(e)=>{setname(e.target.value)}} />
-//         <Props username={name}/>
-//     </div>
-//   </div>;
-// };
-
-// export default App;
-
-// const Props = ({username}) => {
-//   return <div>
-//     <h1>{username}</h1>
-//     This is a Normal Div in the Container One
-//     </div>;
-// };
-
-// export { Props };
+export default App
