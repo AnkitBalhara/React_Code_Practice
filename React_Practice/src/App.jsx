0747ebdfd29a6713cfdf7ea3eca791ce8b534250
Ankit Balhara <ankitbalhara012@gmail.com>
2026-01-02 22:59:40 +0530

    debouncing

--- React_Practice/src/App.jsx
+++ React_Practice/src/App.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useState, useRef } from "react";
 
-const App = () => {
+function App() {
+  const [value, setValue] = useState("");
+
+  const debounce = (fn, delay) => {
+    let timer;
+    return (value) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        fn(value);
+      }, delay);
+    };
+  };
+  const debouncedChange = useRef(
+    debounce((val) => {
+      setValue(val);
+    }, 1000)
+  ).current;
   return (
     <div>
-      JAI SHREE RAM
+      Jai Shree Ram
+      <input type="text" onChange={(e) => debouncedChange(e.target.value)} />
+      <div>{value}</div>
     </div>
   );
-};
+}
 
 export default App;
