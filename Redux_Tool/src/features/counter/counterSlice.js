4b89da98b0a94c3baf16c87b5f2c2806d272ef8f
Ankit Balhara <ankitbalhara012@gmail.com>
2026-01-23 22:47:00 +0530

    practice notes

--- Redux_Tool/src/features/counter/counterSlice.js
+++ Redux_Tool/src/features/counter/counterSlice.js
@@ -1,15 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
 };
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState,
   reducers: {
     increment: (state) => {
-      state.value += 1;  // Redux Toolkit allows direct mutation (via Immer)
+      state.value += 1; 
     },
     decrement: (state) => {
       state.value -= 1;
