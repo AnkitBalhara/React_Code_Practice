124585936baaf231d145459486bb50e819944679
Ankit Balhara <ankitbalhara012@gmail.com>
2026-02-07 12:12:32 +0530

    adding

--- React_Practice/src/App.jsx
+++ React_Practice/src/App.jsx
@@ -1,23 +1,10 @@
-import React, { useCallback, useState } from "react";
-import Child from "./components/Child";
+import React from 'react'
 
-const Parent = () => {
-  const [value, setvalue] = useState(0);
-
-  const add = () => {
-    setvalue((pre) => pre + 1);
-  };
-
-  const sub = useCallback(() => {
-    console.log("useCallback");
-    // Below is the dependecy array,  if you specific that this fn will run when which state/cond. changes so we need to specify that else if we write/console the "value" state it will not be printed.
-  }, []);
+const App = () => {
   return (
     <div>
-      <Child props={sub} />
-      <button onClick={add}>+</button>
-      {value}
     </div>
-  );
-};
-export default Parent;
+  )
+}
+
+export default App
